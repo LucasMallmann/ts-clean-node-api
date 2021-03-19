5b96c67404195fb51374e25e50fde25637243032
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-18 21:06:22 -0300

    refactor: move sut creation to a helper method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,22 +1,38 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
-class ControllerStub implements Controller {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const httpResponse: HttpResponse = {
-      statusCode: 200,
-      body: {}
-    }
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {}
+      }
 
-    return await new Promise(resolve => resolve(httpResponse))
+      return await new Promise(resolve => resolve(httpResponse))
+    }
+  }
+
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut, controllerStub
   }
 }
 
 describe('LogController Decorator', () => {
   test('should call controller handle', async () => {
-    const controllerStub = new ControllerStub()
-
-    const sut = new LogControllerDecorator(controllerStub)
+    const { controllerStub, sut } = makeSut()
 
     const controllerHandleSpy = jest.spyOn(controllerStub, 'handle')
 
