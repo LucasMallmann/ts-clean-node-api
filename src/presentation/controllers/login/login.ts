8e84e33c0b50142228a24e3dc4b4ad220b80cf05
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-24 22:36:35 -0300

    feat: ensure LoginController return 401 when auth fails

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Authentication } from '../../../domain/usecases/authentication'
 import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 import { Controller } from './login-protocols'
@@ -27,7 +27,12 @@ export class LoginController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
-      await this.authentication.auth(email, password)
+      const accessToken = await this.authentication.auth(email, password)
+
+      if (!accessToken) {
+        return unauthorized()
+      }
+
       return await new Promise(resolve => resolve(null as unknown as HttpResponse))
     } catch (error) {
       return serverError(new ServerError(error.stack))
