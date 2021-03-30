ca189e1cc539cd0336065bde7ee1e8ac4808e880
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-30 00:51:41 -0300

    test: ensure BcryptAdapter returns false if bcrypt compare returns false

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -37,6 +37,17 @@ describe('Bcrypt Adapter', () => {
     expect(hashedValue).toBe('hashed_value')
   })
 
+  test('should return true if compare succeeds', async () => {
+    const isValid = await sut.compare('any_value', 'hash_value')
+    expect(isValid).toBe(true)
+  })
+
+  test('should return false if compare fails', async () => {
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const isValid = await sut.compare('any_value', 'hash_value')
+    expect(isValid).toBe(false)
+  })
+
   test('should throw an exception if Bcrypt hash throws', async () => {
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const hashPromise = sut.hash('any_value')
