c1e8f36edea5a5de0bfd5f6628e5417549e3e6d3
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-31 21:29:06 -0300

    feat: add login-factory

---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,26 @@
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication.'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { makeLoginValidation } from './login-validation-factory'
+import env from '../../config/env'
+
+export const makeLoginController = (): Controller => {
+  const salt = 12
+  const logMongoRepository = new LogMongoRepository()
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
+  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
+  return new LogControllerDecorator(loginController, logMongoRepository)
+}
