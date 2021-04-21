70833ec66ce72b6bc39e23391a8239c8b4066884
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-04-21 18:03:57 -0300

    refactor: move decorators factory to a separate folder

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,13 +1,10 @@
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { LoginController } from '../../../../presentation/controllers/login/login-controller'
 import { Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
 import { makeLoginValidation } from './login-validation-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeLoginController = (): Controller => {
-  const dbAuthentication = makeDbAuthentication()
-  const logMongoRepository = new LogMongoRepository()
-  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
-  return new LogControllerDecorator(loginController, logMongoRepository)
+  const loginController = new LoginController(makeDbAuthentication(), makeLoginValidation())
+  return makeLogControllerDecorator(loginController)
 }
