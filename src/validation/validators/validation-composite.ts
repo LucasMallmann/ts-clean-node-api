6fa9c750aa85f3adf9d3f2b4dcdd13eec09f7020
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-05-05 20:10:58 -0300

    feat: add middleware interface

--- src/validation/validators/validation-composite.ts
+++ src/validation/validators/validation-composite.ts
@@ -1,9 +1,7 @@
 import { Validation } from '../../presentation/protocols/validation'
 
 export class ValidationComposite implements Validation {
-  constructor (private readonly validations: Validation[]) {
-
-  }
+  constructor (private readonly validations: Validation[]) {}
 
   validate (input: any): Error | void {
     for (const validation of this.validations) {
