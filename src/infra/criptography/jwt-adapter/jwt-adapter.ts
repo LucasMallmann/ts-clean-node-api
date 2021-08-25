2dd7f3093de0c4dd70c772e763198bb152c63510
Lucas Mallmann <lucas.mallmann@g.globo>
2021-08-24 21:00:55 -0300

    feat: ensure JWTAdapter calls verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -13,6 +13,7 @@ export class JwtAdapter implements Encrypter, Decrypter {
   }
 
   async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secret)
     return await Promise.resolve('oi')
   }
 }
