c645e11c16d724251215a5500a86aaf6ce77d264
Lucas Mallmann <lucas.mallmann@g.globo>
2021-08-24 21:16:52 -0300

    refactor: move controller to another layer

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
+import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
