4617f194ac888bd1582febfb0264a20d48818403
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-09-15 19:05:00 -0300

    refactor: modularize imports

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -1,5 +1,5 @@
 import { ServerError } from '../../../errors'
-import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
